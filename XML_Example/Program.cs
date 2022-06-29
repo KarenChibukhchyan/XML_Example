9b2c2d84487e441d7b93386c85767f7598290be8
Karen Chibukhchyan <karen.chibuxchyan@marktguru.com>
2022-06-29 23:16:43 +0400

    First commit

---
+++ XML_Example/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XML_Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            XDocument xml = XDocument.Load("persons.xml");
+            //get first bob node
+            var persons = xml.Descendants("person");
+            var teachers = persons.SelectMany(person => person.Elements("teacher"));
+            var qualificationsContainer = teachers.SelectMany(teacher => teacher.Elements("qualifications"));
+            var qualifications = qualificationsContainer.SelectMany(qualificationContainer => qualificationContainer.Elements("qualification"));
+            var qualificationIds = qualifications.Select(qualification => qualification.Element("id").Value);
+            var distinctQualificationIds = qualificationIds.Distinct();
+            foreach (var id in distinctQualificationIds)
+            {
+                Console.WriteLine(id);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
